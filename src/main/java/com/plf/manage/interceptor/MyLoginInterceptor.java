48087adbfb5676e4acbfbe7c0742d3d31b913099
Panlf <liangfeng_pan@163.com>
2018-01-13 21:04:36 +0800

    edit hibernate mysqltype

--- src/main/java/com/plf/manage/interceptor/MyLoginInterceptor.java
+++ src/main/java/com/plf/manage/interceptor/MyLoginInterceptor.java
@@ -1,7 +1,7 @@
 package com.plf.manage.interceptor;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.Interceptor;
@@ -12,7 +12,7 @@ public class MyLoginInterceptor implements Interceptor {
 	 * 
 	 */
 	private static final long serialVersionUID = -6398458952217735171L;
-	//private static Logger log=LoggerFactory.getLogger(MyLoginInterceptor.class);
+	private static Logger log=LoggerFactory.getLogger(MyLoginInterceptor.class);
 	public void destroy() {
 		// TODO Auto-generated method stub
 
@@ -25,9 +25,9 @@ public class MyLoginInterceptor implements Interceptor {
 
 	public String intercept(ActionInvocation invocation) throws Exception {
 		// TODO Auto-generated method stub
-		//log.info("当前进入拦截器的Action:{1}",invocation.getAction());
+		log.info("当前进入拦截器的Action:{1}",invocation.getAction());
 		if(invocation.getInvocationContext().getSession().get("existUser")==null && invocation.getAction().toString().indexOf("LoginAction")==-1){
-			//log.info("当前进入拦截器的Action-{1},已经被拦截...",invocation.getAction());
+			log.info("当前进入拦截器的Action-{1},已经被拦截...",invocation.getAction());
 			return "input";
 		}
 		return invocation.invoke();
